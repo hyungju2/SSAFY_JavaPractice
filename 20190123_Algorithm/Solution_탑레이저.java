9a6eac96466acaba471fbec8a0a93c61e95ef1ac
2hyungju <gudwn8439@naver.com>
2019-01-24 08:26:27 +0900

    no message

--- 20190123_Algorithm/Solution_탑레이저.java
+++ 20190123_Algorithm/Solution_탑레이저.java
@@ -7,37 +7,34 @@ public class Solution_탑레이저{
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int num = Integer.parseInt(br.readLine());
 		int []tower = new int[num+1];
 		int []stack = new int[num+1];
-		int i = 1, max = 0;
-		
+		int index = 1, max = 0;
 		String str = br.readLine();
 		StringTokenizer st = new StringTokenizer(str, " ");
 		while(st.hasMoreTokens()){
-			tower[i++] = Integer.parseInt(st.nextToken());
+			tower[index++] = Integer.parseInt(st.nextToken());
 		}
-		
-		for(i = 1; i < num + 1; i++) {
-			if(max < tower[i]) {
-				max = tower[i];
-				stack[i] = 0;
+		for(index = 1; index < num + 1; index++) {
+			
+			if(max < tower[index]) { //본인보다 오른쪽에 더 높은 탑이 있다면, 레이저가 도달하지 않으므로 검사 x
+				max = tower[index]; 
+				stack[index] = 0;
 				continue;
 			}
-			int top = i-1;
-			while(top > 0) {
-				if(tower[i] < tower[top]) {
-					break;
+			int beside = index-1;
+			while(beside > 0) {
+				if(tower[index] < tower[beside]) {
+					break; //만약 옆에 탑이 더 작다면 루프를 빠져나옴.
 				}
-				top--;
+				beside--; //한칸씩 왼쪽으로 이동. 본인보다 높은 높이의 탑이 있다면, beside에 번호가 찍힘.
 			}
-			stack[i] = top;
+			stack[index] = beside; //찍힌 beside를 stack의 index에 저장.
 		}
 			
-		
-		for(i = 1 ; i < num+1 ; i++ ) {
-			System.out.print(stack[i] + " ");
+		for(index = 1 ; index < num+1 ; index++ ) {
+			System.out.print(stack[index] + " "); //전체 stack에 있는 값 출력.
 		}
 		
 	}//end of main
