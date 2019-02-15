3917d6cb80cea898956c388362abba4b80673bdd
hyungju2 <gudwn8439@naver.com>
2019-02-15 19:16:59 +0900

    Solution_2819 최종

--- 20190215_FridayProject/Solution_2819.java
+++ 20190215_FridayProject/Solution_2819.java
@@ -5,28 +5,22 @@ import java.util.StringTokenizer;
 
 public class Solution_2819 {
 
-	public static HashSet<String> hs;
-
+	public static HashSet<String> hs = new HashSet<String>();
 	public static int x = 1;
 	public static int y = 1;
 
-	public static int dx[] = {-1,1,0,0};
-	public static int dy[] = {0,0,-1,1};
+	public static int dx[] = { -1, 1, 0, 0 };
+	public static int dy[] = { 0, 0, -1, 1 };
 	public static int d[][] = new int[6][6];
 	public static int arr[][] = new int[6][6];
+	public static int count;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int testcase = 1; testcase <= T; testcase++) {
-
-		
-			for (int i = 0; i < 6; i++) {
-				for (int j = 0; j < 6; j++) {
-					arr[i][j] = 0;
-				}
-			}
-
+			
+			hs.clear();
 			String line1 = br.readLine();
 			String line2 = br.readLine();
 			String line3 = br.readLine();
@@ -52,34 +46,33 @@ public class Solution_2819 {
 			for (int i = 1; i < 5; i++) {
 				arr[4][i] = Integer.parseInt(tk4.nextToken());
 			}
-			hs = new HashSet<String>();
 			
-			String result = "";
-			for(int i=1; i<5; i++) {
-				for(int j=1; j<5; j++) {
-					result = arr[i][j]+"";
-					DFS(result,0,i-1,j-1,6);
+			String result ="";
+			
+			for (int i = 1; i < 5; i++) {
+				for (int j = 1; j < 5; j++) {
+					dfs(i, j, 7, result);
 				}
 			}
-			
-			System.out.println(hs.size());
+			System.out.println("#"+testcase+" "+hs.size());
 		} // end of testcase
 	}// end of main
 
-	public static void DFS(String str,int depth, int x, int y, int cnt) {
-		if(depth==cnt) {
-			hs.add(str);
+	public static void dfs(int x, int y, int cnt, String result) {
+		if (cnt == 0) {
+			hs.add(result);
+			result = "";
 			return;
-		}
-		for(int i=0; i<4; i++) {
-			int tmpx = x+dx[i];
-			int tmpy = y+dy[i];
-			if(!(tmpx>=1 && tmpy>=1||tmpx<5||tmpy<5)) {
-				continue;
+		} else {
+			for (int i = 0; i < 4; i++) {
+				int tmpx = x + dx[i];
+				int tmpy = y + dy[i];
+
+				if (tmpx >= 1 && tmpx < 5 && tmpy >= 1 && tmpy < 5) {
+					String tmp = arr[tmpx][tmpy]+"";
+					dfs(tmpx, tmpy, cnt-1, result+tmp);
+				}
 			}
-			str += arr[tmpx][tmpy];
-			DFS(str, depth+1, tmpx,tmpy,cnt--);
-			str = str.substring(0,depth+1);
 		}
 	}
 }// end of class
